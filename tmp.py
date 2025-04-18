f47d6304c01cea90d84dc0e6574635ef9d3f8bab
JackOn2690v4 <m15529891017@163.com>
2025-04-18 10:53:34 +0800

    增加16届比赛真题题解

--- tmp.py
+++ tmp.py
@@ -32,3 +32,20 @@
   
 
 
+class Data:
+    def __init__(self, num):
+        self.num = num
+
+    def __lt__(self, other):
+        return other.num + self.num > self.num + other.num
+
+
+n = int(input())
+arr = []
+for i in range(1, n + 1):
+    arr.append(Data(bin(i)[2:]))
+arr.sort(reverse=True)
+ans = ""
+for i in range(len(arr)):
+    ans += arr[i].num
+print(int("0b" + ans, 2))
